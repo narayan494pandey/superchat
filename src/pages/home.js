95b5a4ce39851acb0db767c21403f8e9ee5a9729
Narayan Pandey <npandey722@gmail.com>
2020-11-10 15:23:26 +0530

    opt-form

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,11 +3,30 @@ import {JumbotronContainer} from "../containers/jumbotron";
 import {FooterContainer} from '../containers/footer';
 import {FaqsContainer} from '../containers/faqs';
 import {HeaderContainer} from '../containers/header'
+import {Feature, OptForm } from '../components';
 
 export default function Home() {
     return (
         <>
-            <HeaderContainer />
+            <HeaderContainer>
+            <Feature>
+                <Feature.Title>Unlimited films,TV Programes and more</Feature.Title>
+                <Feature.SubTitle>
+                    Watch anywhere. Cancel at any time.
+                </Feature.SubTitle>
+            
+            </Feature>
+            <OptForm>
+            <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+            <OptForm.Break />
+            <OptForm.Input placeholder="Email Address"/>
+            <OptForm.Button>
+                Try It Now
+            </OptForm.Button>
+           
+    </OptForm>
+ 
+            </HeaderContainer>
             <JumbotronContainer />
             <FaqsContainer />
             <FooterContainer />
