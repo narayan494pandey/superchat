02f8fdd867d882fbc02df1d5706435bbcd8b30a2
Narayan Pandey <npandey722@gmail.com>
2020-11-21 21:28:28 +0530

    profile setup

--- src/containers/profiles.js
+++ src/containers/profiles.js
@@ -13,18 +13,23 @@ export function SelectProfileContainer({user,setProfile}){
             
             </Header>
             <Profiles>
-                <Profiles.Title>Who's watching</Profiles.Title>
-                <Profiles.List>
-                    <Profiles.User >
-                        <Profiles.picture src={user.photoURL} />
-                        <Profiles.Name>{user.displayName}</Profiles.Name>
-                    </Profiles.User>
-                    
-                </Profiles.List>
-               
-            </Profiles>
-            
+            <Profiles.Title>Who's watching</Profiles.Title>
+            <Profiles.List>
+                <Profiles.User 
+                    onClick={() =>
+                        setProfile({
+                            displayName:user.dispalyName,
+                            photoURL:user.photoURL,
+                        })
+                    }
+                >
+                    <Profiles.Picture src={user.photoURL} />
+                    <Profiles.Name>{user.displayName}</Profiles.Name>
+                </Profiles.User>
+                
+            </Profiles.List>
            
+        </Profiles>
         </>
     )
 }
